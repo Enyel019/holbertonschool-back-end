f0a9180d04c795bc39c08761f28a50c1ca59bd2d
Enyel019 <el_dominic01@hotmail.com>
2023-06-18 16:59:12 -0400

    2. Export to JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Gather data from an API."""
+
+import requests
+import sys
+import json
+
+if __name__ == '__main__':
+
+    emp_id = sys.argv[1]
+
+    api_url = "https://jsonplaceholder.typicode.com"
+
+    url = f"{api_url}/users/{emp_id}/todos"
+    resp = requests.get(url)
+    emp_tasks = resp.json()
+
+    url = f"{api_url}/users/{emp_id}"
+    resp = requests.get(url)
+    emp_info = resp.json()
+
+    emp_todos = []
+
+    for task in emp_tasks:
+        emp_todos.append({
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': emp_info.get('username')})
+
+    output_json = {emp_id: emp_todos}
+    with open(f"{emp_id}.json", mode="w") as f:
+        json.dump(output_json, f)
