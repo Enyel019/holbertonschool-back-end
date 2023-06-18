04f647e573f63e2cdefeb2e637ab3481169f6ba1
Enyel019 <el_dominic01@hotmail.com>
2023-06-18 17:37:36 -0400

    new update

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -30,5 +30,5 @@ if __name__ == '__main__':
     ]
     output_json = {emp_id: emp_todos}
 
-    with open(f"{emp_id}.json", mode="a", encoding="utf-8") as f:
+    with open(f"{emp_id}.json", mode="w") as f:
         json.dump(output_json, f)
