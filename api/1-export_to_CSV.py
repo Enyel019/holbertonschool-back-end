ffd0babc42f18fe6adb6528e00e0430f02a4831a
Enyel019 <el_dominic01@hotmail.com>
2023-06-18 10:37:10 -0400

    1. Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Gather data from an API."""
+
+import requests
+import sys
+import csv
+
+if __name__ == '__main__':
+
+    emp_id = sys.argv[1]
+
+    api_url = "https://jsonplaceholder.typicode.com"
+
+    url = f"{api_url}/users/{emp_id}/todos"
+    resp = requests.get(url)
+    emp_tasks = resp.json()
+
+    url = f"{api_url}/users/{emp_id}"
+    resp = requests.get(url)
+    emp_info = resp.json()
+
+    for task in emp_tasks:
+
+        output = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
+            emp_info.get("id"), emp_info.get("username"),
+            task.get("completed"), task.get("title"))
+
+        with open(f"{emp_id}.csv", mode="a", encoding="utf-8") as f:
+            f.write(output)
