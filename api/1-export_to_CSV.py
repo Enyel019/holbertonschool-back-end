b275dc568ce8d1063d66be7c163bc093eb41b8f8
Enyel019 <el_dominic01@hotmail.com>
2023-06-18 16:22:43 -0400

    new update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,7 +3,6 @@
 
 import requests
 import sys
-import string
 
 if __name__ == '__main__':
 
@@ -20,13 +19,10 @@ if __name__ == '__main__':
     emp_info = resp.json()
 
     for task in emp_tasks:
-        alphanumeric_value = ''.join(
-            [char for char in string.ascii_letters + string.digits if char != emp_info["username"]]
-        )
 
         output = "\"{}\",\"{}\",\"{}\",\"{}\"\n".format(
             emp_info.get("id"), emp_info.get("username"),
             task.get("completed"), task.get("title"))
 
-        with open(f"{emp_id}.csv", mode="a", encoding="utf-8") as f:
+        with open(f"{emp_id}.csv", mode="a", newline="") as f:
             f.write(output)
